b3c26052a131b9c81ae6685486c18a5d138aa796
Benjamin Benoit <>
2020-04-08 01:21:15 +0200

    update

---
+++ src/Kitchen.ts
@@ -0,0 +1,23 @@
+export default class Kitchen {
+
+  private _multiplier: number;
+  private _nbCooks: number;
+  private _time: number;
+  private _dish: any[];
+
+  constructor(nbCooks: number) {
+    this._nbCooks = nbCooks;
+    this.introduce();
+  }
+
+  introduce(): void {
+    console.log(`The kitchen is composed of ${this._nbCooks} cooks`);
+  }
+
+  addDish(dish: any): void {
+    this._dish.push(dish);
+    setTimeout( () => {
+      console.log(`${dish} cooked by Kitchen!`)
+    }, dish.bakedTime * 1000 * this._multiplier);
+  }
+}
---
+++ src/dishes.ts
@@ -0,0 +1,56 @@
+export enum DishType {
+    Takoyaki = 1 ,
+    Katsudon = 2 ,
+    Udon = 4 ,
+    Ramen = 8,
+    MatchaCookie = 16
+};
+ 
+export enum Dishsize {
+    S = 1 ,
+    M = 2 ,
+    L = 4 ,
+    XL = 8 ,
+    XXL = 16
+};
+ 
+export enum CommandRegex {
+   type = '[a-zA-Z]+',
+   size = '[a-zA-Z]+',
+   number  = '[0-9]+'
+};
+
+interface Recipe {
+    name: string;
+    bakedin: number;
+    ingredients: string[];
+}
+ 
+  
+export const recipes: Recipe[] = [
+    {
+        name: 'Takoyaki',
+        bakedin: 1,
+        ingredients: ['octopus', 'soja sauce']
+    },
+    {
+        name: 'Katsudon',
+        bakedin: 2,
+        ingredients: ['rice', 'pork', 'eggs']
+    },
+    {
+        name: 'Udon',
+        bakedin: 2,
+        ingredients: ['noodle', 'pork', 'eggs']
+    },
+    {
+        name: 'Ramen',
+        bakedin: 2,
+        ingredients: ['noodle', 'chicken', 'eggs']
+    },
+    {
+        name: 'MatchaCookie',
+        bakedin: 4,
+        ingredients: ['dough', 'matcha', 'chocolate', 'chief love']
+    }
+]
